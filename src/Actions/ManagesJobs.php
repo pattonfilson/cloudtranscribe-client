36406d6ae9c90e6b13b66707960e006aeecdefaf
Craig Rodway <craig@clouddataservice.co.uk>
2021-05-04 11:05:44 +0100

    Support settings

--- src/Actions/ManagesJobs.php
+++ src/Actions/ManagesJobs.php
@@ -45,9 +45,10 @@ trait ManagesJobs
 	 * Create a new job from a file.
 	 *
 	 * @param string|resource $file		Path to file or file resource.
+	 * @param array $settings		Array of supported settings (channel_identification, show_speaker_labels, max_speaker_labels, lang)
 	 *
 	 */
-	public function createJob($file)
+	public function createJob($file, $settings = [])
 	{
 		$fh = null;
 
@@ -63,11 +64,27 @@ trait ManagesJobs
 
 		if ( ! $fh) throw new BaseException("Could not open file for sending.");
 
-		$result = $this->post('jobs', [
-			'multipart' => [
-				[ 'name' => 'file', 'contents' => $fh ],
-			]
-		]);
+		$fields = [];
+
+		$fields[] = ['name' => 'file', 'contents' => $fh];
+
+		if (isset($settings['channel_identification']) && $settings['channel_identification'] === true) {
+			$fields[] = ['name' => 'channel_identification', 'contents' => 'true'];
+		}
+
+		if (isset($settings['show_speaker_labels']) && $settings['show_speaker_labels'] === true) {
+			$fields[] = ['name' => 'show_speaker_labels', 'contents' => 'true'];
+		}
+
+		if (isset($settings['max_speaker_labels']) && $settings['max_speaker_labels'] === true) {
+			$fields[] = ['name' => 'max_speaker_labels', 'contents' => (int) $settings['max_speaker_labels']];
+		}
+
+		if (isset($settings['lang']) && $settings['lang'] === true) {
+			$fields[] = ['name' => 'lang', 'contents' => $settings['lang']];
+		}
+
+		$result = $this->post('jobs', [ 'multipart' => $fields ]);
 
 		return $result ? $result['data'] : false;
 	}
